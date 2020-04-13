692cdbd91da74bb71db03164b62c185f9ac8b0d1
Alisa Chang <laptite@gmail.com>
2020-04-12 22:44:07 -0700

    Install Watir, bundle and cleanup RSpec file

--- spec/selenium/tutorial/ruby_spec.rb
+++ spec/selenium/tutorial/ruby_spec.rb
@@ -1,9 +1,3 @@
 RSpec.describe Selenium::Tutorial::Ruby do
-  it "has a version number" do
-    expect(Selenium::Tutorial::Ruby::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  
 end
